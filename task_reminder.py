fbf19718552a8172b3e544ace043a11fcd7f6949
CalMCGit <caleb.revilla@gmail.com>
2024-08-22 13:46:15 +0800

    Update task_reminder.py
    Adapting to TaskManager functions

--- task_reminder.py
+++ task_reminder.py
@@ -9,54 +9,49 @@ class TaskReminder:
         self.__upcoming_task_reminder = False
 
     def check_due_date(self):
-        #Check due date#
         current_time = datetime.now()
-        #Sends reminder if not yet sent#
         if current_time >= self.__due_date and not self.__reminder_sent:
             self.send_reminder()
             self.__reminder_sent = True
-        
+
     def prompt_overdue_date(self):
-        #When the task is overdue
         now = datetime.now()
-        overdue = now - self.__due_date
-        int_overdue = overdue.days
-        if int_overdue == 1:
+        if now > self.__due_date and not self.__overdue_reminder:
             self.send_overdue_reminder()
             self.__overdue_reminder = True
 
     def prompt_upcoming_tasks(self):
         now = datetime.now()
         upcoming = self.__due_date - now
-        int_upcoming = upcoming.days
-        if int_upcoming == 1:
+        if 0 < upcoming <= timedelta(days=1) and not self.__upcoming_task_reminder:
             self.send_upcoming_tasks()
             self.__upcoming_task_reminder = True
-    
+
     def send_reminder(self):
         #Reminder message#
         print("-------------------------")
-        print(" ")
         print(f"Reminder: The task is due on {self.__due_date.strftime('%m-%d-%Y %H:%M')}!")
-        print(" ")
         print("-------------------------")
-    
+
     def send_overdue_reminder(self):
-        #Sends reminder when task is overdue
+        #Sends reminder when task is overdue#
         print("-------------------------")
-        print(" ")
         print("Reminder: You have an overdue task! Please check your task manager.")
-        print(" ")
         print("-------------------------")
 
     def send_upcoming_tasks(self):
-        #Sends upcoming tasks
+        #Sends upcoming tasks#
         print("-------------------------")
-        print(" ")
-        print("Reminder: Upcoming Task(s)")
-        print("")
         print("Wala pa :P")
-        print(" ")
         print("-------------------------")
-        
-        
+
+
+class TaskWithReminder(Task, TaskReminder):
+    def __init__(self, task_name, description, due_date, priority_level, completion_status):
+        Task.__init__(self, task_name, description, due_date.split()[0], priority_level, completion_status)
+        TaskReminder.__init__(self, due_date)
+
+    def update_and_check_reminder(self):
+        self.check_due_date()
+        self.prompt_overdue_date()
+        self.prompt_upcoming_tasks()
