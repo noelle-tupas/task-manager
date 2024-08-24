6b77b0caa3b4d927ae5e8c9e0ab4999ee4f28189
CalMCGit <caleb.revilla@gmail.com>
2024-08-24 12:44:16 +0800

    Update task_reminder.py
    Just fixed some errors

--- task_reminder.py
+++ task_reminder.py
@@ -44,14 +44,3 @@ class TaskReminder:
         print("-------------------------")
         print("Wala pa :P")
         print("-------------------------")
-
-
-class TaskWithReminder(Task, TaskReminder):
-    def __init__(self, task_name, description, due_date, priority_level, completion_status):
-        Task.__init__(self, task_name, description, due_date.split()[0], priority_level, completion_status)
-        TaskReminder.__init__(self, due_date)
-
-    def update_and_check_reminder(self):
-        self.check_due_date()
-        self.prompt_overdue_date()
-        self.prompt_upcoming_tasks()
