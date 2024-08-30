02c5ff5fa00d178455bc609bd73ede4f8029fbe6
noelle-tupas <noelle.tupas@gmail.com>
2024-08-31 05:59:51 +0800

    Implemented get_overdue_task, get_reminders, added validation to task creation inputs, updated the menu

--- task_manager.py
+++ task_manager.py
@@ -1,38 +1,122 @@
-#from task import Task
-#from reminder import Reminder
+from task import Task
+from reminder import Reminder
+from datetime import datetime
 
 class TaskManager:
     def __init__ (self):
-        '''
-            Initializes the TaskManager instance
-            
-            - The format for due date is MM-DD-YYYY
-            - Priority level can only be high, medium, low
-        '''
         self.__tasks = []
         self.__completed_tasks = []
+        
+    def print_menu (self):
+        '''
+            Prints the menu of the Task Manager app
+        '''
+        print ('---------------------------------------')
+        print ('[1] Add Task')
+        print ('[2] Edit Task')
+        print ('[3] Delete Task')
+        print ('[4] View Tasks')
+        print ('[5] Sort Tasks by Priority')
+        print ('[6] Get Overdue Tasks')
+        print ('[7] Get Reminders')
+        print ('---------------------------------------')
         
     def add_task (self):
         '''
             Allows the user to add a task to the Task Manager
         '''
-        task_name = input ('Task name: ')
-        description = input ('Description: ')
-        due_date = input ('Due date (MM-DD-YYYY): ')
-        priority_level = input ('Priority level (low, medium, high): ')
-        completion_status = input ('Completion level? (%): ')
         
-        # Ask the user if they want a reminder
-        reminder_choice = input('Would you like a reminder? (yes/no): ').strip().lower()
+        '''
+            This code block validates all the user input relevant
+            to the creation of a Task, it will keep on looping until
+            the user provides a valid value.
+        '''
+        while True: 
+            task_name = input ('Enter task name: ').strip()
+            
+            if task_name:
+                break
+            else:
+                print ('Invalid input, task name cannot be empty.')
+                
+        while True: 
+            description = input ('Enter task description: ').strip()
+            
+            if description:
+                break
+            else:
+                print ('Invalid input, task name cannot be empty.')
+                
+        while True: 
+            due_date = input ('Enter task deadline (mm-dd-yyyy): ').strip()
+            
+            if due_date:
+                if datetime.strptime (due_date, '%m-%d-%Y'):
+                    if datetime.strptime (due_date, '%m-%d-%Y') > datetime.now():
+                        break
+                    else:
+                        print ('Invalid input, deadline cannot precede current date.')
+                else:
+                    print ('Invalid input, please follow the format mm-dd-yyyy')
+            else:
+                print ('Invalid input, task name cannot be empty.')
+                
+        while True:
+            priority_level = input ('Enter priority level (low, medium, high): ').strip().lower()
+            
+            if priority_level:
+                if priority_level in ['low', 'medium', 'high']:
+                    break
+                else:
+                    print ('Invalid input, priority level can only be low, medium, or high.')
+            else:
+                print ('Invalid input, priority level cannot be empty.')
+        
+        while True:        
+            completion_status = input ('Enter completion status (%): ')
+            if completion_status:
+                if '%' in completion_status:
+                    completion_status_for_validation = int(completion_status.replace('%', ''))
+                    if completion_status_for_validation >= 0 and completion_status_for_validation <= 100:
+                        break
+                    else:
+                        print ('Invalid input, completion status must be only be 0-100%')
+                else:
+                    print ('Invalid input. Completion status must be suffixed by % (ex. 90%, 30%)')
+            else:
+                print ('Invalid input. Completion status cannot be empty.')
+        
+        '''
+            Verify if the user wants to create a task or a task with reminder
+        '''
+        while True:
+            reminder_choice = input('Would you like a reminder? (yes/no): ').strip().lower()
 
+            if reminder_choice == 'yes' or reminder_choice == 'no':
+                break
+            else:
+                print ('Invalid input, accepted values can only be yes or no')
+
+
+        '''
+            If the user enters yes, then the program will prompt the user for a notification message,
+            if not, then the program proceeds to creating a new Task
+        '''
         if reminder_choice == 'yes':
-            # Create a Reminder object with the provided details
-            new_task = Reminder(Task(task_name, description, due_date, priority_level, completion_status))
+            while True:
+                notification_message = input ("Enter your desired notification message for the reminder.").strip()
+                
+                if notification_message:
+                    break
+                else:
+                    print ('Invalid input, notification message cannot be empty.')
+            
+            new_task = Reminder(task_name, description, due_date, priority_level, completion_status, "Reminder", notification_message)
         else:
-            # Create a Task object with the provided details
-            new_task = Task(task_name, description, due_date, priority_level, completion_status)
+            new_task = Task(task_name, description, due_date, priority_level, "Task", completion_status)
         
         self.__tasks.append(new_task)
+        
         print("Task added successfully!")
 
     def edit_task (self):
@@ -108,7 +192,7 @@ class TaskManager:
                 for key, value in new_task.items():
                     print(f">  {key}: {value}", end='\n')
                 print('---------------------------------------')
-                
+
             print ('\n[***** COMPLETED TASKS *****]')
             for task in self.__completed_tasks:
                 new_task = task.get_task_details()
@@ -155,22 +239,44 @@ class TaskManager:
                 print(f">  {key}: {value}", end='\n')
             print('---------------------------------------')
             
-        
-    # To be implemented
     def get_overdue_tasks (self):
-        print ('Get overdue tasks -- not yet implemented')
+        current_date = datetime.now()
         
-    # Prints the menu
-    def print_menu (self):
-        print ('---------------------------------------')
-        print ('[1] Add Task')
-        print ('[2] Edit Task')
-        print ('[3] Delete Task')
-        print ('[4] View Tasks')
-        print ('[5] Sort Tasks by Priority')
-        print ('[6] Get Overdue Tasks [Not yet available]')
-        print ('---------------------------------------')
-
+        for task in self.__tasks:
+            deadline = task.get_due_date()
+            print (deadline)
+            print (current_date)
+            print (deadline - current_date)
+            if (deadline < current_date):
+                
+                task_number = self.__tasks.index(task)
+                '''
+                    Print task details
+                '''
+                task_details = task.get_task_details()
+                print (f'> Task No: {task_number}')
+                for key, value in task_details.items():
+                    print (f'> {key}: {value}', end='\n')
+                print('---------------------------------------')
+                
+    def get_reminders (self): 
+        ''' 
+            View all tasks with notification/reminders
+        '''
+        task_list = self.__tasks
+        for task in task_list:
+            if task.get_type() == 'Reminder':
+                task_number = task_list.index(task)
+                '''
+                    Print reminder details
+                '''
+                task.display_notification()
+                task_details = task.get_task_details()
+                print (f'> Task No: {task_number}')
+                for key, value in task_details.items():
+                    print (f'> {key}: {value}', end='\n')
+                print('---------------------------------------')
+        
 tm = TaskManager()
 
 while True:
@@ -178,51 +284,25 @@ while True:
     choice = input ("Enter number: ")
     
     if choice == "1":
-        print ('[***** ADD TASK ******************]\n')
+        print ('[***** Add Task ******************]\n')
         tm.add_task()
     elif choice == "2":
-        print ('[***** EDIT TASK *****************]\n')
+        print ('[***** Edit Task *****************]\n')
         tm.edit_task()
     elif choice == "3":
-        print ('[***** DELETE TASK ***************]\n')
+        print ('[***** Delete Task ***************]\n')
         tm.delete_task()
     elif choice == "4":
-        print ('[***** VIEW TASK *****************]\n')
+        print ('[***** View Task *****************]\n')
         tm.view_tasks()
     elif choice == "5":
-        print ('[***** SORT TASK BY PRIORITY *****]\n')
+        print ('[***** Sort Task By Priority *****]\n')
         tm.sort_tasks_by_priority()
     elif choice == "6":
-        print ('[***** GET OVERDUE TASK **********]\n')
+        print ('[***** Get Overdue Task **********]\n')
         tm.get_overdue_tasks()
+    elif choice == "7":
+        print ('[***** Get Reminders *************]\n')
+        tm.get_reminders()
     else:
         print ('Invalid input. Please choose [number] from the menu.')
-    def view_tasks(self):
-        '''
-            Allows the user to view all tasks
-        '''
-        if self.__tasks or self.__completed_tasks:
-            print('\n[***** ONGOING TASKS *****]')
-            for task in self.__tasks:
-                if isinstance(task, Reminder):
-                    task = task.get_task()  # Get the actual Task object
-
-                new_task = task.get_task_details()
-                print('>  Task ' + str(self.__tasks.index(task) + 1) + ':')
-                for key, value in new_task.items():
-                    print(f">  {key}: {value}", end='\n')
-                print('---------------------------------------')
-
-            print('\n[***** COMPLETED TASKS *****]')
-            for task in self.__completed_tasks:
-                if isinstance(task, Reminder):
-                    task = task.get_task()  # Get the actual Task object
-
-                new_task = task.get_task_details()
-                print('>  Task ' + str(self.__completed_tasks.index(task) + 1) + ':')
-                for key, value in new_task.items():
-                    print(f">  {key}: {value}", end='\n')
-                print('---------------------------------------')
-
-        else:
-            print('No available tasks.')
